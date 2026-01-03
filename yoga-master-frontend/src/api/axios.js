4f3f01108e5d478813a4ebdd664a02782fe6f8b5
aadi90392 <adi375461@gmail.com>
2026-01-03 15:12:18 +0530

    Updated

--- yoga-master-frontend/src/api/axios.js
+++ yoga-master-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: "http://localhost:5000"
+ baseURL: "https://yoga-master-api-uiu6.onrender.com"
 });
 
 // Request Interceptor: Har request me Token add karega
